b1792be813fc38aad0ce733ea3d129a77055a576
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-10-23 14:00:04 +0200

    Working on move

--- game/board.go
+++ game/board.go
@@ -2,14 +2,14 @@ package game
 
 type State struct {
 	Board [8][24][2]byte //[color,figure_lowercase] //divided:BlackGrayWhite
-	MovesNext byte
+	MovesNext byte //W G B
 	Castling [2]byte //[color,figure_lowercase]
 	EnPassant [2][2]uint8 //[previousplayer,currentplayer]  [number,letter]
 	HalfmoveClock uint8
 	FullmoveNumber uint16
 }
 
-//func (s State) String() string {
+//func (s *State) String() string {
 //}
 
 type Move struct {
@@ -19,6 +19,13 @@ type Move struct {
 	Before *State
 }
 
+//func (m *Move) String() string {
+//}
+
 func (m *Move) After() *State {
-//	m.Before
+	white := false
+	gray := false
+	black := false
+	switch i:=m.Before.MovesNext {
+	}
 }
