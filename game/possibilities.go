1cef208117a84b23404e85f313039d9003c3084d
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-10-31 18:29:49 +0100

    Working on Diagonal

--- game/possibilities.go
+++ game/possibilities.go
@@ -130,6 +130,7 @@ func (b *Board) Straight(from Pos, to Pos, m MoatsState) (bool, bool) { //(wheth
 }
 
 func (b *Board) Diagonal(from Pos, to Pos, m MoatsState) (bool, bool) {
+	nasz := (*b)[from[0]][from[1]]
 	if from[0] != 0 && from == to {
 		panic("Same square and not the first rank!") //make sure //awaiting email reply
 	}
@@ -150,66 +151,94 @@ func (b *Board) Diagonal(from Pos, to Pos, m MoatsState) (bool, bool) {
 	var canfig bool
 	canfigshort := true
 	canfiglong := true
-	canmoat := true
-	capcheck := true
-	if from[0] == 0 || to[0] == 0 {
-		if from[0] == 0 {
-			mdir := filedirec
-		} else {
+	canmoatshort := true
+	canmoatlong := true
+	capcheckshort := true
+	capchecklong := true
+	if from[0] == 0 || to[0] == 0 { //jeżeli jesteśmy na rank 0
+		if from[0] == 0 { // jeżeli wyjeżdżamy do środka
+			mdir := filedirec     // short jedzie w kierunku mdir, long jedzie w -mdir
+			sprawdzamy := from[1] //
+		} else { // czyli inaczej:  else if to[0]==0  czyli  jeżeli jedziemy na brzeg
 			mdir := -filedirec
+			sprawdzamy := to[1]
 		}
-		switch from[1] {
-		case 0:
-			if mdir == -1 {
-				capcheck = false
-				canmoat = m[0]
-			}
-		case 23:
-			if mdir == 1 {
-				capcheck = false
-				canmoat = m[0]
-			}
+		capchecktemp := true
+		canmoattemp := true
+		var dirtemp int8
+		switch sprawdzamy {
+		case 0: //cross jest jak pojedziemy na minus
+			dirtemp = -1         // jak pojedziesz w tę stronę to wjedziesz w moat'a, jak nie wjedziesz to chyba zero
+			capchecktemp = false // czy wjedziesz w moat'a jak pojedziesz w stronę `dirtemp`, redundantne
+			canmoattemp = m[0]   // czy ten moat jest bridged czy nie
+		case 23: //cross jest jak pojedziemy na plus
+			dirtemp = 1
+			capchecktemp = false
+			canmoattemp = m[0]
 		case 8:
-			if mdir == -1 {
-				capcheck = false
-				canmoat = m[1]
-			}
+			dirtemp = -1
+			capchecktemp = false
+			canmoattemp = m[1]
 		case 7:
-			if mdir == 1 {
-				capcheck = false
-				canmoat = m[1]
-			}
+			dirtemp = 1
+			capchecktemp = false
+			canmoattemp = m[1]
 		case 16:
-			if mdir == -1 {
-				capcheck = false
-				canmoat = m[2]
-			}
+			dirtemp = -1
+			capchecktemp = false
+			canmoattemp = m[2]
 		case 15:
-			if mdir == 1 {
-				capcheck = false
-				canmoat = m[2]
-			}
+			dirtemp = 1
+			capchecktemp = false
+			canmoattemp = m[2]
+		}
+		if dirtemp == mdir {
+			capcheckshort = capchecktemp
+			canmoatshort = canmoattemp
+		} else if dirtemp == -mdir {
+			capchecklong = capchecktemp
+			canmoatlong = canmoattemp
 		}
 	}
-	if short {
-		for i := 1; canfigshort && (i <= przel); i++ {
+	bijemyostatniego := false
+	if short && canmoatshort {
+		for i := 1; canfigshort && (i < przel); i++ {
 			if !((*b)[from[0]+(i*rankdirec)][from[1]+(i*filedirec)].Empty()) {
 				canfigshort = false
 			}
 		}
+		ostatni := (*b)[from[0]+(przel*rankdirec)][from[1]+(przel*filedirec)]
+		if !(ostatni.Empty()) {
+			if ostatni.Color() != nasz.Color() {
+				bijemyostatniego = true
+			} else {
+				canfigshort = false
+			}
+		}
 	}
-	if long {
+	if long && canmoatlong {
 		for i := 1; canfiglong && (i <= (5 - from[0])); i++ {
 			if !((*b)[from[0]+i][from[1]+(i*filedirec)].Empty()) {
 				canfiglong = false
 			}
 		}
-		for i := 0; canfiglong && (i+5-from[0] <= przel); i++ {
+		for i := 0; canfiglong && (i+5-from[0] < przel); i++ {
 			if !((*b)[5-i][from[1]+((5-from[0]+i)*filedirec)].Empty()) {
+				canfiglong = false
+			}
+		}
+		ostatni := (*b)[10-from[0]-przel][from[1]+(przel*filedirec)]
+		if !(ostatni.Empty()) {
+			if ostatni.Color() != nasz.Color() {
+				bijemyostatniego = true
+			} else {
 				canfiglong = false
 			}
 		}
 	}
 	canfig = canfigshort || canfiglong
+	if canfigshort && canfiglong {
+		canmoat = canmoatshort || canmoatlong
+	} // dalej: co jeśli jedno z nich? rozpatrywać przypadki tylko short i tylko long
 	return cantech && canfig && canmoat, capcheck
 }
