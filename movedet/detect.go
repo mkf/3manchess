ed2fa3662336ce71bd317ca01fa52c49fc1e4373
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-11-13 01:50:24 +0100

    Working on move detection (comments)

--- movedet/detect.go
+++ movedet/detect.go
@@ -4,4 +4,5 @@ import "github.com/ArchieT/3manchess/game"
 
 func WhatMove(bef *game.State, aft *game.Board) (*game.Move, *game.State, error) {
 	//yep, it's right! all over, again!
+	//but now... with concurrency!
 }
