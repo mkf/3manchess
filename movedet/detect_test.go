dbc7c31c64eab8bf5e8a32eb9b8074a66f6b2135
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-11-13 21:46:01 +0100

    Working on tests. Got panic().

---
+++ movedet/detect_test.go
@@ -0,0 +1,35 @@
+package movedet
+
+import "testing"
+import "github.com/ArchieT/3manchess/game"
+
+var g []game.Move
+
+//var simplyillegal []testentity
+
+func init() {
+	g = []game.Move{
+		{game.Pos{1, 0}, game.Pos{3, 0}, &game.NEWGAME},
+	}
+	temp, _ := g[0].After()
+	g = append(g, game.Move{game.Pos{3, 0}, game.Pos{4, 0}, temp})
+
+	//simplyillegal = []testentity{
+	//	{Pos{3,0},Pos{5,0},g[0].After()}
+	//}
+
+}
+
+func TestGood(t *testing.T) {
+	for _, pair := range g {
+		temp, _ := pair.After()
+		v, w, err := WhatMove(pair.Before, temp.Board)
+		if err != nil {
+			t.Error("For", pair, "got an error", err, "and value", v, w)
+		} else if (v.From != pair.From) || (v.To != pair.To) {
+			t.Error("For", pair, "got", v, w, "expected", temp)
+		}
+	}
+}
+
+//func TestSimplyIllegal
