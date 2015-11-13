79668f60684f970f614fee1f79c752ae12607666
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2015-11-13 01:44:24 +0100

    Started working on move detection

---
+++ movedet/detect.go
@@ -0,0 +1,7 @@
+package movedet
+
+import "github.com/ArchieT/3manchess/game"
+
+func WhatMove(bef *game.State, aft *game.Board) (*game.Move, *game.State, error) {
+	//yep, it's right! all over, again!
+}
