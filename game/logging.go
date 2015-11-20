e72d2c37f07e1badb97e40b199b31405c17596dc
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-11-20 20:38:39 +0100

    Forgot to add logging.go ;)

---
+++ game/logging.go
@@ -0,0 +1,17 @@
+package game
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	Debug        = log.New(os.Stderr, "DEBUG ", log.LstdFlags)
+	BodyTrace    = log.New(ioutil.Discard, "BODY:TRACE ", log.LstdFlags)
+	EndgameTrace = log.New(ioutil.Discard, "ENDGAME:TRACE ", log.LstdFlags)
+	MoveTrace    = log.New(ioutil.Discard, "MOVE:TRACE ", log.LstdFlags)
+	PossibTrace  = log.New(ioutil.Discard, "POSSIB:TRACE ", log.LstdFlags)
+	SimplTrace   = log.New(ioutil.Discard, "SIMPL:TRACE ", log.LstdFlags)
+	StateTrace   = log.New(ioutil.Discard, "STATE:TRACE ", log.LstdFlags)
+)
