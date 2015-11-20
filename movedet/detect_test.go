afead3b101358aca4a1881db054e861a42d6b4f9
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-11-20 08:48:58 +0100

    Working on tests

--- movedet/detect_test.go
+++ movedet/detect_test.go
@@ -7,22 +7,18 @@ var g []game.Move
 
 //var simplyillegal []testentity
 
-func init() {
-	g = []game.Move{
-		{game.Pos{1, 0}, game.Pos{3, 0}, &game.NEWGAME},
-	}
-	temp, _ := g[0].After()
-	g = append(g, game.Move{From: game.Pos{3, 0}, To: game.Pos{4, 0}, Before: temp})
+var first = game.Move{{game.Pos{1, 0}, game.Pos{3, 0}, &game.NEWGAME}}
+var temp, _ = first.After()
+var g = []game.Move{first, game.Move{From: game.Pos{3, 0}, To: game.Pos{4, 0}, Before: temp}}
 
-	//simplyillegal = []testentity{
-	//	{Pos{3,0},Pos{5,0},g[0].After()}
-	//}
-
-}
+//simplyillegal = []testentity{
+//	{Pos{3,0},Pos{5,0},g[0].After()}
+//}
 
 func TestGood(t *testing.T) {
 	for _, pair := range g {
 		temp, _ := pair.After()
+
 		v, w, err := WhatMove(pair.Before, temp.Board)
 		if err != nil {
 			t.Error("For", pair, "got an error", err, "and value", v, w)
