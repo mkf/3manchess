5a49bccd253d3b811a0a5b4be0ba8ecdde2e0c03
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-11-21 18:27:51 +0100

    move::comments

--- game/move.go
+++ game/move.go
@@ -101,7 +101,7 @@ func (b *Board) CheckChecking(who Color, pa PlayersAlive) bool { //true if in ch
 	}
 	for i = 0; i < 6; i++ {
 		for j = 0; j < 24; j++ {
-			if !((b.AnyPiece(Pos{i, j}, where, DEFMOATSSTATE, FALSECASTLING, DEFENPASSANT)) || ((*b)[i][j].NotEmpty && pa[(*b)[i][j].Color().UInt8()])) { //Bug(ArchieT): Checks color despite the fact there is nothing there.
+			if !((b.AnyPiece(Pos{i, j}, where, DEFMOATSSTATE, FALSECASTLING, DEFENPASSANT)) || ((*b)[i][j].NotEmpty && pa[(*b)[i][j].Color().UInt8()])) {
 				MoveTrace.Println("CheckChecking: TRUE!")
 				return true
 			}
