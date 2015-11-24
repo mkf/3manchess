ec662bfb46181c4833d28f39a87974f69f10e702
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-11-24 08:16:00 +0100

    Enabled MoveTrace

--- game/logging.go
+++ game/logging.go
@@ -14,7 +14,7 @@ var (
 	//EndgameTrace is the not saved by default log of endgame.go without errors
 	EndgameTrace = log.New(ioutil.Discard, "ENDGAME:TRACE ", log.LstdFlags)
 	//MoveTrace is the not saved by default log of move.go without errors
-	MoveTrace = log.New(ioutil.Discard, "MOVE:TRACE ", log.LstdFlags)
+	MoveTrace = log.New(os.Stderr, "MOVE:TRACE ", log.LstdFlags) //enabled
 	//PossibTrace is the not saved by default log of possibilities.go without errors
 	PossibTrace = log.New(ioutil.Discard, "POSSIB:TRACE ", log.LstdFlags)
 	//SimplTrace is the not saved by default log of simple.go without errors
