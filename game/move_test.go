b8c851ae5f66fccf67c189c4349da0ae54268822
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-11-24 08:12:21 +0100

    Made *tests* properly handle check false-positive

--- game/move_test.go
+++ game/move_test.go
@@ -7,7 +7,7 @@ func TestSimpleGenNoPanic(t *testing.T) {
 	temp, err := first.After()
 	t.Log("first.After ", temp)
 	if err != nil {
-		t.Log("Error first.After ", err)
+		t.Error("Error first.After ", err)
 	}
 	second := Move{From: Pos{3, 0}, To: Pos{4, 0}, Before: temp}
 	stemp, err := second.After()
@@ -15,16 +15,22 @@ func TestSimpleGenNoPanic(t *testing.T) {
 	if err != nil {
 		t.Log("Error second.After ", err)
 	}
+	if err == nil {
+		t.Error("Error second.After IS NIL!")
+	}
 	third := Move{From: Pos{3, 8}, To: Pos{4, 8}, Before: temp}
 	ttemp, err := third.After()
 	t.Log("third.After ", ttemp)
 	if err != nil {
 		t.Log("Error third.After ", err)
 	}
+	if err == nil {
+		t.Error("Error third.After IS NIL!")
+	}
 	forth := Move{From: Pos{1, 8}, To: Pos{3, 8}, Before: temp}
 	ftemp, err := forth.After()
 	t.Log("forth.After ", ftemp)
 	if err != nil {
-		t.Log("Error forth.After ", err)
+		t.Error("Error forth.After ", err)
 	}
 }
