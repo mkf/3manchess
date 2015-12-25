3ec670c9bd211a42c1e7d8725dfd3af4cc32f53c
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-12-25 13:09:55 +0100

    Working on Player and deveng

--- interface/deveng/deveng.go
+++ interface/deveng/deveng.go
@@ -1,1 +1,40 @@
 package deveng
+
+import "github.com/ArchieT/3manchess/game"
+import "github.com/ArchieT/3manchess/player"
+import "fmt"
+
+type Developer string
+
+func (p *Developer) Initialize(*player.Gameplay) <-chan error {
+	errchan := make(chan error)
+
+	return errchan
+}
+
+func (p *Developer) HeyItsYourMove(s *game.State, hurry <-chan bool) *game.Move {
+
+	return
+}
+
+func (p *Developer) HeySituationChanges(m *game.Move, aft *game.State) {
+	return
+}
+
+func (p *Developer) HeyYouLost(*game.State) {
+	fmt.Println("%s has lost", *p)
+}
+
+func (p *Developer) HeyYouWonOrDrew(*game.State) {
+	fmt.Println("%s has won/drew", *p)
+}
+
+type GivingUpError string
+
+func (g GivingUpError) Error() string {
+	return string(g)
+}
+
+func (g GivingUpError) IGaveUp() string {
+	return string(g)
+}
--- player/player.go
+++ player/player.go
@@ -4,11 +4,12 @@ import "github.com/ArchieT/3manchess/game"
 
 //Player is either AI or a human via some UI
 type Player interface {
-	Initialize() <-chan error
-	HeyItsYourMove(*game.Move, <-chan bool) *game.Move //that channel is for signalling to hurry up
+	Initialize(*Gameplay) <-chan error
+	HeyItsYourMove(*game.State, <-chan bool) *game.Move //that channel is for signalling to hurry up
 	HeySituationChanges(*game.Move, *game.State)
 	HeyYouLost(*game.State)
 	HeyYouWonOrDrew(*game.State)
+	String() string
 }
 
 //Gameplay is a list of players and the current gamestate pointer
@@ -19,8 +20,14 @@ type Gameplay struct {
 	*game.State
 }
 
+type GivingUpError interface {
+	error
+	IGaveUp() string
+}
+
 //NewGame returns a new Gameplay
-func NewGame(w *Player, g *Player, b *Player) Gameplay {
+func NewGame(w *Player, g *Player, b *Player) *Gameplay {
 	ns := game.NewState()
-	return Gameplay{w, g, b, &ns}
+	gp := Gameplay{w, g, b, &ns}
+	return &gp
 }
