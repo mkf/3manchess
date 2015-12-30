3913256228aa2d0166a3132eb8d194f743e8f7cd
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-12-30 23:58:00 +0100

    Added sitvalues/ai_test.go

---
+++ ai/sitvalues/ai_test.go
@@ -0,0 +1,18 @@
+package sitvalues
+
+import "testing"
+import "github.com/ArchieT/3manchess/game"
+import "time"
+import "log"
+
+func TestSimpleGenNoPanic(t *testing.T) {
+	var a AIPlayer
+	hurry := make(chan bool)
+	newgame := game.NewState()
+	go func() {
+		time.Sleep(time.Minute)
+		hurry <- true
+	}()
+	move := a.HeyItsYourMove(&newgame, hurry)
+	log.Println(move)
+}
