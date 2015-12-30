f26c35b425e7b7698266d9228bf0d081ce413bc9
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-12-30 22:16:42 +0100

    Working on ai/sitvalues

---
+++ ai/sitvalues/ai.go
@@ -0,0 +1,42 @@
+package sitweights
+
+import "github.com/ArchieT/3manchess/game"
+import "github.com/ArchieT/3manchess/simple"
+import "github.com/ArchieT/3manchess/player"
+import "sync"
+import "sync/atomic"
+import "fmt"
+
+type AIPlayer struct {
+	errchan           chan error
+	ErrorChan         chan<- error
+	hurry             chan bool
+	HurryChan         chan<- bool
+	FixedPrecision    float64
+	OwnedToThreatened float64
+	gp                *player.Gameplay
+	waiting           bool
+}
+
+func (a *AIPlayer) Initialize(gp *player.Gameplay) {
+	errchan := make(chan error)
+	a.errchan = errchan
+	a.ErrorChan = errchan
+	hurry := make(chan bool)
+	a.hurry = hurry
+	a.HurryChan = hurry
+	a.gp = gp
+	go func() {
+		for b := range a.errchan {
+			panic(b)
+		}
+	}()
+}
+
+func (a *AIPlayer) HurryChannel() chan<- bool {
+	return a.HurryChan
+}
+
+func (a *AIPlayer) ErrorChannel() chan<- error {
+	return a.ErrorChan
+}
