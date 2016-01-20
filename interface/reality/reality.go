738ee699a0b69e41a6a1565308ab755c9cfa5bab
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-20 22:28:33 +0100

    Working on Reality

--- interface/reality/reality.go
+++ interface/reality/reality.go
@@ -15,8 +15,13 @@ type Reality struct {
 
 type RealPlayer struct {
 	*Reality
-	color game.Color
-	Name  string
+	color     game.Color
+	Name      string
+	errchan   chan error
+	ErrorChan chan<- error
+	HurryChan chan<- bool
+	hurry     chan bool
+	gp        *player.Gameplay
 }
 
 func NewReality() *Reality {
@@ -25,10 +30,37 @@ func NewReality() *Reality {
 
 func (re *Reality) MakePlayers(who ...game.Color) map[game.Color]*RealPlayer {
 	ourm := make(map[game.Color]*RealPlayer)
-	var ourp RealPlayer
+	var ourp *RealPlayer
 	for _, c := range who {
-		ourp = RealPlayer{re, c, c.String()}
-		ourm[c] = &ourp
+		ourp = new(RealPlayer)
+		ourp.Reality = re
+		ourp.Name = c.String() + "_real"
+		ourm[c] = ourp
 	}
 	return ourm
+}
+
+func (p *RealPlayer) Initialize(gp *player.Gameplay) {
+	errchan := make(chan error)
+	p.errchan = errchan
+	hurry := make(chan bool)
+	p.hurry = hurry
+	p.gp = gp
+	p.ErrorChan = errchan
+	p.HurryChan = hurry
+}
+
+func (p *RealPlayer) String() string { return p.Name }
+
+func (p *RealPlayer) ErrorChannel() chan<- error { return p.ErrorChan }
+
+func (p *RealPlayer) HurryChannel() chan<- bool { return p.HurryChan }
+
+func (p *RealPlayer) HeyItsYourMove(s *game.State, hurryi <-chan bool) *game.Move {
+	go func() {
+		for {
+			p.hurry <- <-hurryi
+		}
+	}()
+	return nil
 }
