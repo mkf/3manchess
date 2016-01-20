5daa9d4eb920a3c4bc9718a5726bf5b8d0eae46c
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-20 21:41:14 +0100

    Started writing Reality

--- interface/reality/reality.go
+++ interface/reality/reality.go
@@ -4,7 +4,8 @@ import "github.com/ArchieT/3manchess/interface/reality/machine"
 import "github.com/ArchieT/3manchess/interface/reality/camget"
 import "github.com/ArchieT/3manchess/game"
 import "github.com/ArchieT/3manchess/player"
-import "log"
+
+//import "log"
 
 type Reality struct {
 	camget.View
@@ -12,4 +13,20 @@ type Reality struct {
 	BlackIsOnWhitesRight bool
 }
 
-//func
+type RealPlayer struct {
+	*Reality
+	color game.Color
+	Name  string
+}
+
+func NewReality() *Reality {
+	return new(Reality)
+}
+
+func (re *Reality) MakePlayers(who ...game.Color) map[game.Color]*RealPlayer {
+	ourm := make(map[game.Color]*RealPlayer)
+	for _, c := range who {
+		ourm[c] = RealPlayer{re, c, c.String()}
+	}
+	return ourm
+}
