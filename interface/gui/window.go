9ae01347ca1db9198731c0630ec42f4482f39c5c
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-21 16:39:19 +0100

    Started writing, so far does not make sense

--- interface/gui/window.go
+++ interface/gui/window.go
@@ -1,1 +1,24 @@
 package gui
+
+import "github.com/andlabs/ui"
+
+type Window struct {
+}
+
+func NewWindow() *Window {
+	err := ui.Main(func() {
+		name := ui.NewEntry()
+		boardspace := ui.NewVerticalBox()
+		boardspace.Append(ui.NewLabel("test"), false)
+		window := ui.NewWindow("Hello", 200, 100, false)
+		window.SetChild(boardspace)
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
+}
