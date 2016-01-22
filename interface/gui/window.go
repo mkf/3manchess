4d93dc40e15085777bb742ad0cb017fadc997fb7
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-22 12:29:30 +0100

    Switching to QML in go code as well

--- interface/gui/window.go
+++ interface/gui/window.go
@@ -1,45 +1,10 @@
 package gui
 
-import "github.com/andlabs/ui"
+import "gopkg.in/qml.v1"
+import "log"
 
-type Window struct {
+type GUI struct {
 }
 
-type BoardHandler struct {
-	*Window
-}
-
-func (bh *BoardHandler) Draw(a *ui.Area, dp *ui.AreaDrawParams) {
-}
-
-func (bh *BoardHandler) MouseEvent(a *ui.Area, me *ui.AreaMouseEvent) {
-}
-
-func (bh *BoardHandler) MouseCrossed(a *ui.Area, left bool) {
-}
-
-func (bh *BoardHandler) DragBroken(a *ui.Area) {
-}
-
-func (bh *BoardHandler) KeyEvent(a *ui.Area, ke *ui.AreaKeyEvent) (handled bool) {
-	return false
-}
-
-func NewWindow() *BoardHandler {
-	ourwindow := new(Window)
-	bh := BoardHandler{ourwindow}
-	err := ui.Main(func() {
-		boardarea := ui.NewScrollingArea(&bh, 150, 150)
-		window := ui.NewWindow("Hello", 300, 200, false)
-		window.SetChild(boardarea)
-		window.OnClosing(func(*ui.Window) bool {
-			ui.Quit()
-			return true
-		})
-		window.Show()
-	})
-	if err != nil {
-		panic(err)
-	}
-	return &bh
+func NewGUI() (*GUI, error) {
 }
