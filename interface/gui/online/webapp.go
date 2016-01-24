c346cafa51e50ca16d889265d3733892fc64f8b9
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-01-24 02:51:32 +0100

    Working on the webapp

--- interface/gui/online/webapp.go
+++ interface/gui/online/webapp.go
@@ -1,13 +1,25 @@
 package online
 
-import "github.com/ArchieT/3manchess/game"
-import "github.com/ArchieT/3manchess/interface/gui"
-import "fmt"
+//import "github.com/ArchieT/3manchess/game"
+//import "github.com/ArchieT/3manchess/interface/gui"
+//import "fmt"
 import "net/http"
 import "google.golang.org/appengine"
-import "google.golang.org/appengine/user"
+
+//import "google.golang.org/appengine/user"
 import "google.golang.org/appengine/datastore"
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
+	c.Deadline() //just a placeholder senseless and probably harmful, just delete and forget this line
+}
+
+func PropertiesFromMaps(m ...map[string]interface{}) []datastore.Property {
+	p := make([]datastore.Property, 0, 4)
+	for n := range m {
+		for k := range m[n] {
+			p = append(p, datastore.Property{Name: k, Value: m[n][k], NoIndex: false, Multiple: true})
+		}
+	}
+	return p
 }
