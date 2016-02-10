141276b9e7c35cb4340826c744facd54da40455c
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-02-10 22:54:13 +0100

    Working on servers

--- server/server.go
+++ server/server.go
@@ -3,7 +3,7 @@ package server
 import "github.com/ArchieT/3manchess/player"
 
 type Server interface {
-	SaveGP(*player.Gameplay) (key string)
+	SaveGPD(*player.Gameplay) (key string)
 	OpenGP(key string) *player.Gameplay
 	GetDerived(key string) (derived_keys []string)
 }
