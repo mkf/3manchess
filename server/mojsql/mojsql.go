b96cb540b0563c6bb95125368b46d2f495db4804
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-02-11 23:10:11 +0100

    Working on mariadb table layout

--- server/mojsql/mojsql.go
+++ server/mojsql/mojsql.go
@@ -16,12 +16,63 @@ func (m *MojSQL) Initialize(username string, password string, database string) e
 	if err != nil {
 		return err
 	}
-	res, err := conn.Exec("CREATE TABLE if not exists 3mangp (id bigint auto_increment primary key, state bigint, white bigint, gray bigint, black bigint, date datetime)")
+	res, err = conn.Exec(`
+	CREATE TABLE if not exists 3manst (
+		id bigint auto_increment primary key, 
+		board blob not null, 
+		moatzero boolean not null, 
+		moatone boolean not null,
+		moattwo boolean not null,
+		movesnext tinyint not null, 
+		castling tinyint not null, 
+		enpasprevrow tinyint not null, 
+		enpasprevfile tinyint not null,
+		enpascurrow tinyint not null, 
+		enpascurfile tinyint not null,
+		halfmoveclock tinyint not null, 
+		fullmovenumber smallint not null, 
+		alivewhite bool not null,
+		alivegray bool not null,
+		aliveblack bool not null
+	) ENGINE = InnoDB`)
 	log.Println(res)
 	if err != nil {
 		return err
 	}
-	res, err = conn.Exec("CREATE TABLE if not exists 3manst (id bigint auto_increment primary key, board blob, moats bool array(3), movesnext shortint, castling shortint, enpasprev shortint array(2), enpascur shortint array(2), halfmoveclock shortint, fullmovenumber int, alive bool array(3))")
+	res, err = conn.Exec(`
+	create table if not exists 3manplayer (
+		id bigint auto_increment primary key,
+		whoami varchar(100) not null,
+		name varchar(100) not null,
+		precise double,
+		coefficient double,
+		pawnpromotion tinyint
+	) engine = InnoDB`)
+	log.Println(res)
+	if err != nil {
+		return err
+	}
+	res, err := conn.Exec(`
+	CREATE TABLE if not exists 3mangp (
+		id bigint auto_increment primary key, 
+		state bigint not null, 
+		white bigint not null, 
+		gray bigint not null, 
+		black bigint not null, 
+		date datetime not null,
+		constraint
+			foreign key (state) references 3manst (id)
+			on update restrict,
+		constraint
+			foreign key (white) references 3manplayer (id)
+			on update restrict,
+		constraint
+			foreign key (gray) references 3manplayer (id)
+			on update restrict,
+		constraint
+			foreign key (black) references 3manplayer (id)
+			on update restrict
+	) ENGINE = InnoDB`)
 	log.Println(res)
 	return err
 }
