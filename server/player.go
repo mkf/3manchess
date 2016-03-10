21baebb01c80d3850d5206450883247014c6d41e
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-03-11 00:38:50 +0100

    ServerPlayer, MoveIt

--- server/player.go
+++ server/player.go
@@ -11,18 +11,18 @@ type ServerPlayer struct {
 	errchan chan error
 	wwme    bool
 	move    chan *game.Move
-	sitret  chan player.SituationChange
+	sitret  chan *game.State
 }
 
-func (sp *ServerPlayer) GiveMove(m *game.Move) <-chan player.SituationChange {
+func (sp *ServerPlayer) GiveMove(m *game.Move) <-chan *game.State {
 	sp.move <- m
 	return sp.sitret
 }
 
-func (sp *ServerPlayer) Initialize(*player.Gameplay) { sp.sitret = make(chan player.SituationChange) }
+func (sp *ServerPlayer) Initialize(*player.Gameplay) { sp.sitret = make(chan *game.State) }
 func (sp *ServerPlayer) ErrorChannel() chan<- error  { return sp.errchan }
-func (sp *ServerPlayer) HurryChannel() chan<- error {
-	c := make(chan error)
+func (sp *ServerPlayer) HurryChannel() chan<- bool {
+	c := make(chan bool)
 	go func() {
 		for {
 			<-c
@@ -36,7 +36,24 @@ func (sp *ServerPlayer) String() string            { return fmt.Sprint(sp.ID) }
 func (sp *ServerPlayer) HeyYouWon(*game.State)     {}
 func (sp *ServerPlayer) HeyYouDrew(*game.State)    {}
 func (sp *ServerPlayer) HeyYouLost(*game.State)    {}
-func (sp *ServerPlayer) HeySituationChanges(m *game.Move, s *game.State) {
-	sp.sitret <- player.SituationChange{m, s}
+func (sp *ServerPlayer) HeySituationChanges(_ *game.Move, s *game.State) {
+	sp.sitret <- s
 }
 func (sp *ServerPlayer) HeyItsYourMove(_ *game.State, _ <-chan bool) *game.Move { return <-sp.move }
+
+func MoveIt(m *game.Move, ids [3]int64) *game.State {
+	w := ServerPlayer{ID: ids[0]}
+	g := ServerPlayer{ID: ids[1]}
+	b := ServerPlayer{ID: ids[2]}
+	p := player.Gameplay{
+		Players: map[game.Color]player.Player{game.White: &w, game.Gray: &g, game.Black: &b},
+		State:   m.Before}
+	w.Initialize(&p)
+	g.Initialize(&p)
+	b.Initialize(&p)
+	r := map[game.Color]*ServerPlayer{game.White: &w, game.Gray: &g, game.Black: &b}
+	go func() {
+		p.Turn()
+	}()
+	return <-r[m.Before.MovesNext].GiveMove(m)
+}
