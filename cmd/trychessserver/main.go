ed6d49f5925a485f830eb403ed276b428ce830c0
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-03-11 21:54:24 +0100

    Added trychessserver

---
+++ cmd/trychessserver/main.go
@@ -0,0 +1,16 @@
+package main
+
+//import "github.com/ArchieT/3manchess/server"
+import "github.com/ArchieT/3manchess/server/mojsql"
+import "github.com/ArchieT/3manchess/multi"
+import "fmt"
+import "log"
+
+func main() {
+	var mmm mojsql.MojSQL
+	var a, b, c string
+	fmt.Scanf("%s %s %s", &a, &b, &c)
+	log.Println(mmm.Initialize(a, b, c))
+	mul := multi.Multi{&mmm}
+	mul.Run()
+}
