c2f49c94ab579fb5b266568e60da0497744d8b7b
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2016-03-11 17:02:52 +0100

    Working on multi

--- multi/multiweb.go
+++ multi/multiweb.go
@@ -20,7 +20,7 @@ type Multi struct {
 	server.Server
 }
 
-func Run() {
+func (mu *Multi) Run() {
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8082", router))
 }
@@ -46,20 +46,19 @@ func Logger(inner http.Handler, name string) http.Handler {
 
 type Routes []Route
 
-var routes = Routes{
-	{"Index", "GET", "/", Index},
-	{"APIIndex", "GET", "/api", APIIndex},
-	{"APISignUp", "POST", "/api/signup", APISignUp},
-	{"APIAuth", "POST", "/api/newbot", APINewBot},
-	{"APICreate", "POST", "/api/addgame", APIAddGame},
-	{"APIPlay", "GET", "/api/play/{gameId}", APIPlay},
-	{"APITurn", "POST", "/api/play/{gameId}", APITurn},
-	{"APIState", "GET", "/api/state/{stateId}", APIState},
-	{"APIMove", "GET", "/api/move/{moveId}", APIMove},
-}
-
-func NewRouter() *mux.Router {
+func (mu *Multi) NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
+	var routes = Routes{
+		{"Index", "GET", "/", mu.APIIndex},
+		{"APIIndex", "GET", "/api", mu.APIIndex},
+		{"APISignUp", "POST", "/api/signup", mu.APISignUp},
+		{"APIAuth", "POST", "/api/newbot", mu.APINewBot},
+		{"APICreate", "POST", "/api/addgame", mu.APIAddGame},
+		{"APIPlay", "GET", "/api/play/{gameId}", mu.APIPlay},
+		{"APITurn", "POST", "/api/play/{gameId}", mu.APITurn},
+		{"APIState", "GET", "/api/state/{stateId}", mu.APIState},
+		{"APIMove", "GET", "/api/move/{moveId}", mu.APIMove},
+	}
 	for _, router := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
@@ -69,16 +68,22 @@ func NewRouter() *mux.Router {
 	return router
 }
 
-type SignUp struct {
+func (mu *Multi) APIIndex(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "Index is here") }
+
+type SignUpPost struct {
 	Login  string `json:"login"`
 	Passwd string `json:"passwd"`
 	Name   string `json:"name"`
 }
 
-func APIIndex(w http.ResponseWriter, r *http.Request) { fmt.Fprintln(w, "Index is here") }
+type SignUpGive struct {
+	User   int64  `json:"userid"`
+	Player int64  `json:"playerid"`
+	Auth   []byte `json:"authkey"`
+}
 
-func APISignUp(w http.ResponseWriter, r *http.Request) {
-	var su SignUp
+func (mu *Multi) APISignUp(w http.ResponseWriter, r *http.Request) {
+	var su SignUpPost
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
 		panic(err)
@@ -93,4 +98,17 @@ func APISignUp(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 	}
+	var gi SignUpGive
+	uu, pp, aa, ee := mu.Server.SignUp(su.Login, su.Passwd, su.Name)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(ee); err != nil {
+			panic(err)
+		}
+	}
+	gi.User = uu
+	gi.Player = pp
+	gi.Auth = aa
+	//ciag dalszy, wg poradnika restful thenewstack
 }
