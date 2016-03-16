96b879d74b016759796b674f29dc262bfed8f3c8
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-03-16 20:34:02 +0100

    VFTPGen test logging

--- game/vftpgen_test.go
+++ game/vftpgen_test.go
@@ -3,10 +3,11 @@ package game
 //© Copyright 2015-2016 Michał Krzysztof Feiler & Paweł Zacharek
 
 import "testing"
+import "log"
 
 func TestVFTPGen_newgame(t *testing.T) {
 	newstate := NewState()
-	for _ = range VFTPGen(&newstate) {
-		;
+	for r := range VFTPGen(&newstate) {
+		log.Println(r)
 	}
 }
