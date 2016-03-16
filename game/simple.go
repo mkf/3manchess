de7324e3fcbec1c58d146192b2d00c3c9e16b523
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-03-16 19:21:59 +0100

    Moved VFTPGen to vftpgen.go

--- game/simple.go
+++ game/simple.go
@@ -35,30 +35,6 @@ func (a *ACFT) G() (bool, FromTo) {
 	return a.OK(), FromTo(*a)
 }
 
-//VFTPGen : generates all valid FromToProm, given the game state
-func VFTPGen(gamestate *State) <-chan FromToProm {
-	all_valid := make(chan FromToProm)
-	go func() {
-		var oac ACFT
-		for oac.OK() {
-			ft := FromTo(oac)
-			move := Move{ft.From(), ft.To(), gamestate, Queen}
-			if _, err := move.After(); err == nil {
-				all_valid <- FromToProm{ft, Queen}
-				fig := (*gamestate).Board.GPos(ft.From()).Fig
-				if fig.FigType == Pawn && fig.PawnCenter && ft.From()[0] == 1 {
-					all_valid <- FromToProm{ft, Rook}
-					all_valid <- FromToProm{ft, Bishop}
-					all_valid <- FromToProm{ft, Knight}
-				}
-			}
-			oac.P()
-		}
-		close(all_valid)
-	}()
-	return all_valid
-}
-
 //ACP — all combinations pos
 type ACP Pos
 
