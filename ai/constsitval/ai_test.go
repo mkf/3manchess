2883301c89e15755d9f4b5b087238928429df0d7
Subc2 <subc2@wp.pl>
2016-03-17 09:59:59 +0100

    Started working on ai/constsitval

---
+++ ai/constsitval/ai_test.go
@@ -0,0 +1,20 @@
+package sitvalues
+
+//© Copyright 2015-2016 Michał Krzysztof Feiler & Paweł Zacharek
+
+import "testing"
+import "github.com/ArchieT/3manchess/game"
+import "time"
+import "log"
+
+func TestSimpleGenNoPanic(t *testing.T) {
+	var a AIPlayer
+	hurry := make(chan bool)
+	newgame := game.NewState()
+	go func() {
+		time.Sleep(time.Minute)
+		hurry <- true
+	}()
+	move := a.HeyItsYourMove(&newgame, hurry)
+	log.Println(move)
+}
