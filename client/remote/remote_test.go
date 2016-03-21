3abf243eba26fccc39c2652d02e6df8c41fae0ae
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-03-21 21:26:14 +0100

    Working on remote_test

--- client/remote/remote_test.go
+++ client/remote/remote_test.go
@@ -6,9 +6,10 @@ import "github.com/ArchieT/3manchess/client"
 import "flag"
 import "os"
 
-//import "github.com/ArchieT/3manchess/ai/constsitval"
+import "github.com/ArchieT/3manchess/ai/constsitval"
 import "github.com/ArchieT/3manchess/multi"
 import "time"
+import "fmt"
 
 var c *client.Client
 
@@ -45,10 +46,51 @@ func TestNew_ai3(t *testing.T) {
 	var mgpp multi.GameplayPost
 	mgpp.Date = time.Now()
 	mgpp.State = *ns
+	var botsconf [3]constsitval.AIConfig
+	botsconf[0].OwnedToThreatened = 4.0
+	botsconf[1].OwnedToThreatened = 5.0
+	botsconf[2].OwnedToThreatened = 6.0
+	var botsau [3]multi.NewBotGive
+	for bno, bco := range botsconf {
+		var bbi, bbp int64
+		var bba []byte
+		if bb, _, err := c.NewBot(multi.NewBotPost{"constsitval-demotesting", multi.Authorization{u, a}, fmt.Sprint("bot", bno), bco.Byte()}); err == nil {
+			bbi, bbp, bba = bb.Botid, bb.PlayerID, bb.AuthKey
+		} else {
+			t.Log(err)
+			bbi = bno + 1
+			bbb, _, err := c.BotKey(multi.BotKeyGetting{bbi, multi.Authorization{u, a}})
+			if err != nil {
+				t.Fatal(err)
+			}
+			bbp, bba = bbb.ID, bbb.AuthKey
+		}
+		botsau[bno] = multi.NewBotGive{bbi, bbp, bba}
+	}
+	mgpp.White = &botsau[0].PlayerID
+	mgpp.Gray = &botsau[1].PlayerID
+	mgpp.Black = &botsau[2].PlayerID
 	gpg, _, err := c.AddGame(mgpp)
 	t.Log(*gpg)
 	if err != nil {
 		t.Fatal(err)
 	}
-	//	b1,_,err:=
+	echn := make(chan error)
+	endchn := make(chan bool)
+	go func() {
+		for u := range echn {
+			t.Log(u)
+		}
+	}()
+	var nboty [3]*G
+	for bno := range game.COLORS {
+		var aii constsitval.AIPlayer
+		aii.Conf = botsconf[bno]
+		yg, err := New(c, aii, game.COLORS[bno], multi.Authorization{botsau[bno].PlayerID, botsau[bno].AuthKey}, *gpg, echn, endchan)
+		if err != nil {
+			t.Log(err)
+		}
+		nboty[bno] = yg
+	}
+	t.Log("end", <-endchn)
 }
