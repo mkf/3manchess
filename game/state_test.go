77da98ced0038767de661c0722fd3756b1f27c17
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2016-04-01 08:59:17 +0200

    Testing StateData

--- game/state_test.go
+++ game/state_test.go
@@ -1,11 +1,13 @@
 package game
 
+import "testing"
+
 func TestFromData_newgame(t *testing.T) {
 	ns := NewState()
 	nd := ns.Data()
 	var xs State
 	xs.FromData(nd)
-	if !xs.Equal(ns) {
+	if !xs.Equal(&ns) {
 		t.Fatal(ns, xs)
 	}
 }
