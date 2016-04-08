a493157749144c25ac9f5511aa9ebd5a806cba8c
Subc2 <subc2@wp.pl>
2016-04-09 01:12:01 +0200

    Revert "Merge branch 'master' into Subc2-master"
    This reverts commit 815949f87e1eb05f7726060e5adfdde675a51dbe, reversing
    changes made to 1f898e2dc5d7bc781f0173adfb8991ff94892570.

--- game/board.go
+++ game/board.go
@@ -137,6 +137,17 @@ var COLORS = [3]Color{White, Gray, Black}
 //FIRSTRANKNEWGAME : first rank, from 0mod8 to 7mod8
 var FIRSTRANKNEWGAME = [8]FigType{Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook}
 
+//ALLPOS : all valid positions
+var ALLPOS [6 * 24]Pos
+
+func allposinit() { //initialize ALLPOS
+	for y := 0; y < 6; y++ {
+		for x := 0; x < 24; x++ {
+			ALLPOS[y*24 + x] = Pos{int8(y), int8(x)}
+		}
+	}
+}
+
 //BOARDFORNEWGAME — a newgame board
 var BOARDFORNEWGAME Board //newgame board
 
