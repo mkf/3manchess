02ffd26f173d3dc48999fb5688e06add213b226c
Subc2 <subc2@wp.pl>
2016-04-09 23:36:07 +0200

    Initialize ALLFROMTO by default

--- game/state.go
+++ game/state.go
@@ -262,6 +262,7 @@ var NEWGAME State
 
 func init() { //initialize module pseudoconstants
 	allposinit()
+	allfromtoinit()
 	boardinit()
 	NEWGAME = State{&BOARDFORNEWGAME, DEFMOATSSTATE, White, DEFCASTLING, DEFENPASSANT, HalfmoveClock(0), FullmoveNumber(1), DEFPLAYERSALIVE}
 }
