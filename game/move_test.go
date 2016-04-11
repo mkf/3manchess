5de5b07f74a90231ded262d5a8e9284ec85d20be
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-04-11 21:23:40 +0200

    added comment in plat129 array in move_test

--- game/move_test.go
+++ game/move_test.go
@@ -17,7 +17,7 @@ var plat129 []FromTo = []FromTo{
 	{Pos{2, 0}, Pos{2, 8}},
 	{Pos{0, 9}, Pos{2, 10}},
 	{Pos{0, 16}, Pos{2, 16}},
-	{Pos{2, 8}, Pos{2, 15}},
+	{Pos{2, 8}, Pos{2, 15}}, //incorrect afaik, has to return error
 	/*{Pos{0, 14}, Pos{2, 13}},
 	{Pos{2, 16}, Pos{2, 15}},
 	{Pos{0, 1}, Pos{2, 0}},
