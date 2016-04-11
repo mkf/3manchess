9fde9667184f2a63ef3b72d1fa2fb8d32d8e4cc5
Subc2 <subc2@wp.pl>
2016-04-11 16:03:20 +0200

    Removed ACFT type

--- game/simple.go
+++ game/simple.go
@@ -2,39 +2,6 @@ package game
 
 //© Copyright 2015-2016 Michał Krzysztof Feiler & Paweł Zacharek
 
-//ACFT — all combinations fromto
-type ACFT FromTo
-
-//P adds one to our FromTo
-func (a *ACFT) P() {
-	(*a)[0][0]++
-	if (*a)[0][0] != 6 {
-		return
-	}
-	(*a)[0][0] = 0
-	(*a)[0][1]++
-	if (*a)[0][1] != 24 {
-		return
-	}
-	(*a)[0][1] = 0
-	(*a)[1][0]++
-	if (*a)[1][0] != 6 {
-		return
-	}
-	(*a)[1][0] = 0
-	(*a)[1][1]++
-}
-
-//OK checks whether it is correct
-func (a *ACFT) OK() bool {
-	return (*a)[1][1] != 24
-}
-
-//G checks whether it is correct and returns it
-func (a *ACFT) G() (bool, FromTo) {
-	return a.OK(), FromTo(*a)
-}
-
 //ACP — all combinations pos
 type ACP Pos
 
