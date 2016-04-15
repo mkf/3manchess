b9f7d0ed9eaff2492279117ab094ff12e415bf06
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2016-04-15 19:12:12 +0200

    Access-Control-Allow-Origin *

--- multi/multiweb.go
+++ multi/multiweb.go
@@ -36,6 +36,7 @@ func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			inner.ServeHTTP(w, r)
 			log.Printf("%s\t%s\t%s\t%s", r.Method, r.RequestURI, name, time.Since(start))
 		})
